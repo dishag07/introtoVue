e91869757d211e883fd6d82229a54d4e87b91bd8
dishag07 <144719310+dishag07@users.noreply.github.com>
2024-12-12 16:33:32 +0000

    Attribute binding

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-onst product = 'Socks'
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
